1611d56c431470740a691d545530de34015522e3
shalevgerbi <57968427+shalevgerbi@users.noreply.github.com>
2021-08-18 19:57:12 +0300

    a Netflix-Clone

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAIDmowLHyi7lgPIN3kD-PwX8H_SUfYj44",
+    authDomain: "netflix-clone-1a2fa.firebaseapp.com",
+    projectId: "netflix-clone-1a2fa",
+    storageBucket: "netflix-clone-1a2fa.appspot.com",
+    messagingSenderId: "861851680635",
+    appId: "1:861851680635:web:5c325d9e4db250fc4f87de"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth };
+  export default db;
